0f3c07910034f108a6821129ba41310fced2d5cd
RubenFern <rubenferval@hotmail.com>
2026-08-12 13:32:27 +0200

    00.0004.0000: atribución de la foto y contador de picos sin cambios
    Cuatro columnas nullable en peaks (image_author, image_license, image_license_url,
    image_credit_url) para el crédito CC-BY-SA de las fotos de Commons, y peaks_unchanged en
    peak_ingestion_runs para que una ejecución idempotente no salga 0/0/0.
    
    Generado desde la migración AddPeakImageAttributionAndRunDiff de peak-service.

---
+++ schema/00.0004.0000/001-add-peak-image-attribution.sql
@@ -0,0 +1,46 @@
+﻿START TRANSACTION;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260812110522_AddPeakImageAttributionAndRunDiff') THEN
+    ALTER TABLE peaks ADD image_author character varying(200);
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260812110522_AddPeakImageAttributionAndRunDiff') THEN
+    ALTER TABLE peaks ADD image_credit_url character varying(500);
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260812110522_AddPeakImageAttributionAndRunDiff') THEN
+    ALTER TABLE peaks ADD image_license character varying(60);
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260812110522_AddPeakImageAttributionAndRunDiff') THEN
+    ALTER TABLE peaks ADD image_license_url character varying(500);
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260812110522_AddPeakImageAttributionAndRunDiff') THEN
+    ALTER TABLE peak_ingestion_runs ADD peaks_unchanged integer NOT NULL DEFAULT 0;
+    END IF;
+END $EF$;
+
+DO $EF$
+BEGIN
+    IF NOT EXISTS(SELECT 1 FROM "__EFMigrationsHistory" WHERE "MigrationId" = '20260812110522_AddPeakImageAttributionAndRunDiff') THEN
+    INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+    VALUES ('20260812110522_AddPeakImageAttributionAndRunDiff', '10.0.10');
+    END IF;
+END $EF$;
+COMMIT;
+
